5884015a7111e01b06589e388c99ede25ace9430
Rajiv Paul <74105638+Rajiv-paul@users.noreply.github.com>
2024-08-20 15:02:10 +0000

    notes of datatyes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,12 @@
+"use strict";// treats all Js code as newer version
+
+// number => 2^53 (range)
+// bigint
+// null => standalone value
+// undefined => value not assigned
+// symbol => used for uniqueness
+
+// object
+
+// typeof null is object and typeof undefined is undefined 
+

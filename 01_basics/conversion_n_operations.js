331ecf3caf030aa5dbc787f2501a228db4336900
Rajiv Paul <74105638+Rajiv-paul@users.noreply.github.com>
2024-08-20 15:19:04 +0000

    conversion of datatypes

---
+++ 01_basics/conversion_n_operations.js
@@ -0,0 +1,33 @@
+let scores = "79"
+
+console.log(typeof scores);
+
+let valInNumber = Number(scores)
+
+console.log(typeof valInNumber);
+
+
+// after converison scenarios
+// "79" => 79
+// "79abc" => NaN(Not a Number)
+// true => 1, false => 0
+
+
+let isLogedIn = 1
+
+let booleanIsLogedIn = Boolean(isLogedIn)
+
+console.log(booleanIsLogedIn);
+
+
+// 1 => true, 0=> false
+// "" => false
+// "rajiv" => true
+
+let smNum = "07"
+let stringNum = String(smNum)
+console.log(stringNum);
+console.log(typeof stringNum);
+
+
+
